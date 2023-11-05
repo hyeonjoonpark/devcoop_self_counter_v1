fee65200c7e87e39b8e6131fe09a798a31b8e2a5
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-06 08:33:19 +0900

    FEAT : 결제페이지 버튼

---
+++ lib/presentation/ui/components/all_items_count.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/devcoop_colors.dart';
+
+Widget allItemsCount(BuildContext context) {
+  return Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center, // Column을 세로로 중앙에 정렬
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center, // Row를 가로로 중앙에 정렬
+          children: [
+            Container(
+              padding: EdgeInsets.only(right:304, top: 24),
+              child: const Text(
+                "총 상품 개수 및 합계",
+                style: TextStyle(
+                  color: DevCoopColors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.only(left: 330, right: 64, top: 24),
+              child: const Text(
+                "1",
+                style: TextStyle(
+                  color: DevCoopColors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.only(right: 24, top: 24),
+              child: const Text(
+                "1400",
+                style: TextStyle(
+                  color: DevCoopColors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
--- lib/presentation/ui/view/payments_page.dart
+++ lib/presentation/ui/view/payments_page.dart
@@ -1,4 +1,6 @@
+import 'package:counter/presentation/ui/components/all_items_count.dart';
 import 'package:counter/presentation/utils/devcoop_colors.dart';
+import 'package:counter/presentation/utils/self_counter_button.dart';
 import 'package:flutter/material.dart';
 import 'package:counter/presentation/ui/components/user_selected_items.dart';
 
@@ -43,6 +45,48 @@ class _MyAppState extends State<Payments> {
             ),
 
             userSelectedItems(context),
+
+            Container(
+              width: screenWidth * 0.7,
+              margin: EdgeInsets.only(top: 440),
+              child: Divider( // Divider 위젯을 사용하여 가로 줄을 그립니다.
+                color: Colors.black, // 가로 줄의 색상을 지정할 수 있습니다.
+                thickness: 4, // 가로 줄의 두께를 조절할 수 있습니다.
+              ),
+            ),
+
+            allItemsCount(context),
+
+            Container(
+              margin: EdgeInsets.only(left: 776, top: 40),
+              child: CounterElevatedButton(
+                onPressed: () {
+
+                },
+                text: "계산하기",
+                buttonStyle: ButtonStyle(
+                  backgroundColor:
+                  MaterialStateProperty.all(DevCoopColors.primary),
+
+                  elevation: MaterialStateProperty.all(0), // 그림자 비활성화
+
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24.0), // 테두리를 둥글게 설정
+                  )),
+
+                  side: MaterialStateProperty.all(
+                    const BorderSide(
+                      color: DevCoopColors.primary, // 원하는 테두리 색상 설정
+                      width: 2.0, // 테두리 두께 설정
+                    ),
+                  ),
+                ),
+                textStyle: const TextStyle(
+                  fontSize: 40,
+                  color: DevCoopColors.black,
+                ),
+              ),
+            ),
           ],
         ),
       ),
