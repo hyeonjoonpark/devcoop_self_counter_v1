f42a387129523a3d499c657f0d1aa90aea89085f
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-06 00:55:54 +0900

    FEAT : Button 분리

--- lib/presentation/ui/view/main_page.dart
+++ lib/presentation/ui/view/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:counter/presentation/utils/devcoop_colors.dart';
+import 'package:counter/presentation/utils/self_counter_button.dart';
 
 void main() {
   runApp(MaterialApp(home: MyHome()));
@@ -21,14 +22,17 @@ class _MyAppState extends State<MyHome> {
           Center(
             child: Container(
               margin: EdgeInsets.only(top: 600), // 사용자 지정 단위로 마진 설정
-              child: const Text(
-                'touch to start',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w900,
-                  color: DevCoopColors.black,
-                ),
-              ),
+              child: CounterElevatedButton(
+                  onPressed: () {},
+                  text: "touch to start",
+                  buttonStyle: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(DevCoopColors.white),
+                  ),
+                  textStyle: TextStyle(
+                    fontSize: 40,
+                    color: DevCoopColors.black,
+                  )),
             ),
           )
         ],
--- lib/presentation/utils/devcoop_colors.dart
+++ lib/presentation/utils/devcoop_colors.dart
@@ -4,4 +4,5 @@ class DevCoopColors {
   static const Color primary = Color(0xFFFFC20C);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
+  static const Color transparent = Color(0x00000000);
 }
