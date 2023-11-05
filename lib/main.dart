84cde8a92ddb6f33869d5a79427421e45b0167b2
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-05 22:01:18 +0900

    SET : 프로젝트 세팅

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:counter/utils/devcoop_colors.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  MyApp();
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(top: 600), // 사용자 지정 단위로 마진 설정
+              child: const Text(
+                'touch to start',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900,
+                  color: DevCoopColors.black,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/devcoop_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class DevCoopColors {
+  static const Color primary = Color(0xFFFFC20C);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+}
