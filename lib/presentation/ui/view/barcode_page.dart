fc6d465faab8332d832f17296038ab951eb3f510
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-12 00:06:16 +0900

    TODO : 로그인 에러 수정

--- lib/presentation/ui/view/barcode_page.dart
+++ lib/presentation/ui/view/barcode_page.dart
@@ -1,20 +1,31 @@
-import 'package:counter/presentation/utils/devcoop_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:counter/model/barcode_api.dart';
+import 'package:counter/presentation/utils/devcoop_colors.dart';
+import 'package:counter/presentation/utils/self_counter_button.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Barcode(),
-  ));
+  runApp(const MaterialApp(home: BarcodeApp()));
+}
+
+class BarcodeApp extends StatelessWidget {
+  const BarcodeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Barcode(),
+    );
+  }
 }
 
 class Barcode extends StatefulWidget {
-  Barcode();
+  const Barcode({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
+  _BarcodeState createState() => _BarcodeState();
 }
 
-class _MyAppState extends State<Barcode> {
+class _BarcodeState extends State<Barcode> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -24,9 +35,9 @@ class _MyAppState extends State<Barcode> {
         child: Column(
           children: [
             Container(
-              margin: const EdgeInsets.only(top: 150),
+              margin: const EdgeInsets.only(top: 100),
               child: const Text(
-                "학생 바코드를\n 스캔해주세요",
+                "학생 바코드를 스캔해주세요",
                 style: TextStyle(
                   fontSize: 30,
                   color: DevCoopColors.black,
@@ -35,11 +46,32 @@ class _MyAppState extends State<Barcode> {
               ),
             ),
             Container(
-              margin: EdgeInsets.only(top: 100),
+              margin: const EdgeInsets.only(top: 100),
               child: Image.asset(
                 'assets/barcode.png',
                 width: 300,
                 height: 300,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  top: screenHeight * 0.1, bottom: screenHeight * 0.1),
+              child: CounterElevatedButton(
+                buttonStyle: ButtonStyle(
+                  alignment: Alignment.center,
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(DevCoopColors.primary),
+                ),
+                onPressed: () {
+                  print("로그인 버튼 클릭");
+                  login(context);
+                },
+                text: '로그인',
+                textStyle: TextStyle(
+                  color: DevCoopColors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                ),
               ),
             ),
           ],
