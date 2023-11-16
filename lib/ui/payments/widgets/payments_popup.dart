0d40a67c64335e345225637c2928e28f28e7d65b
hyeonjoonpark <pjjoon1379@gmail.com>
2023-11-16 14:34:26 +0900

    FEAT: popup

--- lib/ui/payments/widgets/payments_popup.dart
+++ lib/ui/payments/widgets/payments_popup.dart
@@ -1,13 +1,38 @@
 import 'package:counter/ui/_constant/theme/devcoop_colors.dart';
 import 'package:counter/ui/_constant/theme/devcoop_text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 AlertDialog paymentsPopUp(BuildContext context) {
+  Future<void> removeUserData() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.remove('codeNumber');
+      prefs.remove('pin');
+      prefs.remove('point');
+      prefs.remove('studentName');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void navigateToNextPage() {
+    // Use Navigator to push a new page
+    Get.toNamed('/');
+  }
+
+  // Delayed navigation after 5 seconds
+  Future.delayed(const Duration(seconds: 3), () {
+    removeUserData();
+    navigateToNextPage();
+  });
+
   return AlertDialog(
     content: Container(
       width: 520,
       height: 320,
-      child:  Column(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
